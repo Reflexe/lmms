b70a7a5db2b1d7bde1ea0237a22819250fba9ffb
Shmuel H <shmuelhazan0@gmail.com>
2017-11-08 11:15:52 +0200

    Track -> SampleTrack: Do not save start time offset for empty SampleTCOs.

--- src/core/Track.cpp
+++ src/core/Track.cpp
@@ -1024,7 +1024,9 @@ void TrackContentObjectView::mouseMoveEvent( QMouseEvent * me )
 					m_tco->movePosition( t );
 					m_trackView->getTrackContentWidget()->changePosition();
 					m_tco->changeLength( m_tco->length() + ( oldPos - t ) );
-					sTco->setStartTimeOffset( sTco->startTimeOffset() + ( oldPos - t ) );
+
+					if (! sTco->isEmpty ())
+						sTco->setStartTimeOffset( sTco->startTimeOffset() + ( oldPos - t ) );
 				}
 			}
 		}
