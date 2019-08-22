02b4ac1b984f7e82839b7dab34398cc7f469278d
Shmuel H <shmuelhazan0@gmail.com>
2019-08-22 17:40:17 +0300

    CMake->CreateTempFile: Use hash for unique name instead of a counter.

--- cmake/modules/DefineInstallVar.cmake
+++ cmake/modules/DefineInstallVar.cmake
@@ -14,10 +14,13 @@ function(DEFINE_INSTALL_VAR)
 
     # install(CODE) does not support generator expression in ver<3.14
     if(VAR_GENERATOR_EXPRESSION AND ${CMAKE_VERSION} VERSION_LESS "3.14.0")
-        include(CreateTempFilePath)
-        CreateTempFilePath(OUTPUT_VAR file_path)
-        file(GENERATE OUTPUT "${file_path}" CONTENT "${VAR_CONTENT}")
-        install(CODE "file(READ ${file_path} \"${VAR_NAME}\")")
+        if(MSVC)
+            message(FATAL_ERROR "Installing is not supported with msvc and cmake<3.14")
+        endif()
+
+        include(CreateTempFile)
+        CreateTempFilePath(OUTPUT_VAR file_path TAG "${VAR_NAME}" CONTENT "${VAR_CONTENT}")
+        install(CODE "file(READ \"${file_path}\" \"${VAR_NAME}\")")
     else()
         cmake_policy(SET CMP0087 NEW)
         install(CODE "set(\"${VAR_NAME}\" \"${VAR_CONTENT}\")")
